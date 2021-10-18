20b9625d95b0b6341838bf3a6e56d648e81b92d8
Dang Hoang <hoangdv@icts.vn>
2021-10-18 22:49:20 +0700

    ui app

--- WebBlock/ContentView.swift
+++ WebBlock/ContentView.swift
@@ -1,21 +1,103 @@
 //
 //  ContentView.swift
-//  WebBlock
+//  WebProtector
 //
 //  Created by Tuấn Việt on 17/10/2021.
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    func rateApp() {
+        SKStoreReviewController.requestReview()
+    }
+    func openContactToUs() {
+        let email = "maituanviet.work@gmail.com"
+        if let url = URL(string: "mailto:\(email)") {
+          if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url)
+          } else {
+            UIApplication.shared.openURL(url)
+          }
+        }
+    }
+    
+    func shareApp() {
+        let myWebsite = NSURL(string:"http://itunes.apple.com/app/id1590726316")
+           let activityVC = UIActivityViewController(activityItems: [myWebsite], applicationActivities: nil)
+           UIApplication.shared.windows.first?.rootViewController?.present(activityVC, animated: true, completion: nil)
+       }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+        GeometryReader { metrics in
+        ZStack{
+            VStack{
+                Spacer()
+                HStack{
+                    Image("bottomBgBold")
+                    Spacer()
+                }
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity)
+
+            VStack{
+                Image("icApp")
+                Text("WebProtectoror")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.black)
+                    .padding(.bottom, metrics.size.height * 0.06)
+                NavigationLink(destination:Tutorial()){
+                    btnView(label: "Tutorial", icImg: "ic1home")
+                }
+               
+                Button{
+                    rateApp()
+                } label: {
+                    btnView(label: "Rate us", icImg: "ic2home")
+                }
+                Button{
+                    openContactToUs()
+                } label: {
+                    btnView(label: "Contact us", icImg: "ic3home")
+                }
+                Button{
+                    shareApp()
+                } label: {
+                    btnView(label: "Share", icImg: "ic4home")
+                }
+           
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .top)
+        }//ZStack
+        }//GeometryReader
+        .frame(maxWidth:.infinity,maxHeight: .infinity)
+        .background(Color.white).ignoresSafeArea(edges:.bottom)
+        }//NavigationView
+        .navigationBarHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct btnView: View {
+    let label: String!
+    let icImg: String!
+    var body: some View {
+        ZStack{
+            Image("bgBtn")
+                .resizable()
+            VStack{
+                HStack{
+                    Text(label)
+                        .foregroundColor(Color.white)
+                    Spacer()
+                    Image(icImg)
+                }
+            }
+            .padding(.horizontal,20)
+        }
+        .frame(maxWidth:200 , maxHeight: 60)
     }
 }
+
---
+++ WebBlock/Tutorial.swift
@@ -0,0 +1,218 @@
+//
+//  Tutorial.swift
+//  HighLightApp
+//
+//  Created by itsactuallyme on 07/10/2021.
+//
+
+import SwiftUI
+
+struct Tutorial: View {
+    var body: some View {
+        TabView {
+            Step1()
+            Step2()
+            Step3()
+        }  .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .background(Color.white).ignoresSafeArea(.all)
+    }
+}
+
+struct Step1: View {
+    var body: some View {
+        ZStack{
+            VStack{
+                Spacer()
+                HStack{
+                    Image("bottomBg")
+                    Spacer()
+                }
+    
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity)
+            VStack(spacing: 20){
+                VStack{
+                    Text("STEP 1")
+                          .foregroundColor(Color("SECONDARY"))
+                          .font(.largeTitle)
+                          .fontWeight(.bold)
+                    Text("OPEN EXTENSION SETTINGS")
+                          .foregroundColor(Color("SECONDARY"))
+                          .font(.title3)
+                          .fontWeight(.bold)
+                }.padding(.bottom,30)
+           
+                VStack(alignment:.leading,spacing: 20){
+                    HStack{
+                        Image("ic1step1")
+                        HStack{
+                            Text("Open the")
+                                .foregroundColor(Color("PRIMARY"))
+                            Text("Settings App")
+                                .foregroundColor(Color("PRIMARY"))
+                                .fontWeight(.bold)
+                        }
+                    }
+                    HStack{
+                        Image("ic2step1")
+                        HStack{
+                            Text("Tap")
+                                .foregroundColor(Color("PRIMARY"))
+                            Text("Safari")
+                                .foregroundColor(Color("PRIMARY"))
+                                .fontWeight(.bold)
+                        }
+                    }
+                    HStack{
+                        Image("ic3step1")
+                        HStack{
+                            Text("Tap")
+                                .foregroundColor(Color("PRIMARY"))
+                            Text("Extensions")
+                                .foregroundColor(Color("PRIMARY"))
+                                .fontWeight(.bold)
+                        }
+                    }
+                    HStack{
+                        Image("icApp")
+                            .resizable()
+                            .frame(maxWidth:30,maxHeight: 30)
+                        HStack{
+                            Text("Tap")
+                                .foregroundColor(Color("PRIMARY"))
+                            Text("WebProtectoror")
+                                .foregroundColor(Color("PRIMARY"))
+                                .fontWeight(.bold)
+                        }
+                    }
+                }.padding(.horizontal,20)
+               
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .top)
+            .padding(.top,100)
+            
+        }
+        .frame(maxWidth:.infinity,maxHeight: .infinity)
+        .background(Color.white).ignoresSafeArea(.all)
+    }
+}
+
+struct Step2: View {
+    var body: some View {
+        ZStack{
+            VStack{
+                Spacer()
+                HStack{
+                    Image("bottomBg")
+                    Spacer()
+                }
+    
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity)
+            VStack(spacing: 20){
+                VStack{
+                    Text("STEP 2")
+                          .foregroundColor(Color("SECONDARY"))
+                          .font(.largeTitle)
+                          .fontWeight(.bold)
+                    Text("ENABLE SAFARI EXTENSION")
+                          .foregroundColor(Color("SECONDARY"))
+                          .font(.title3)
+                          .fontWeight(.bold)
+                }.padding(.bottom,30)
+                VStack(alignment:.center){
+                    VStack(alignment:.leading,spacing: 20){
+                        HStack{
+                            Image("ic1step2")
+                            Text("Enable ")
+                                    .foregroundColor(Color("PRIMARY"))
+                                    .fontWeight(.bold) + Text("WebProtectoror")
+                                    .foregroundColor(Color("PRIMARY"))
+                        }
+                        HStack{
+                            Image("ic2step2")
+                            Text("Tap the permission for ")
+                                    .foregroundColor(Color("PRIMARY")) + Text("\"All Websites\"")
+                                    .foregroundColor(Color("PRIMARY"))
+                                    .fontWeight(.bold)
+                        }
+                        HStack{
+                            Image("ic3step2")
+                            Text("Select ")
+                                    .foregroundColor(Color("PRIMARY")) + Text("Allow")
+                                    .foregroundColor(Color("PRIMARY"))
+                                    .fontWeight(.bold)
+                        }
+                    }.padding(.horizontal,20)
+                        .padding(.bottom,40)
+                    VStack(alignment:.leading,spacing: 20){
+                        Text("Steps above will allow")
+                            .foregroundColor(Color("PRIMARY")) + Text(" WebProtectoror ")
+                            .foregroundColor(Color("PRIMARY"))
+                            .fontWeight(.bold) + Text("to access all websites.")
+                            .foregroundColor(Color("PRIMARY"))
+                        Text("You can enable this app on a per-site basis by lauching the extension from Safari.")
+                            .foregroundColor(Color("PRIMARY"))
+                    }
+                }
+                .padding(.horizontal,30)
+               
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .top)
+            .padding(.top,100)
+            
+        }
+        .frame(maxWidth:.infinity,maxHeight: .infinity)
+        .background(Color.white).ignoresSafeArea(.all)
+    }
+}
+
+struct Step3: View {
+    var body: some View {
+        ZStack{
+            VStack{
+                Spacer()
+                HStack{
+                    Image("bottomBg")
+                    Spacer()
+                }
+    
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity)
+            VStack(spacing: 20){
+                VStack{
+                    Text("YOU'RE ALL SET")
+                          .foregroundColor(Color("SECONDARY"))
+                          .font(.largeTitle)
+                          .fontWeight(.bold)
+                }.padding(.bottom,30)
+                Image("icDone")
+                Text("Open")
+                    .foregroundColor(Color("PRIMARY")) + Text(" Safari ")
+                    .foregroundColor(Color("PRIMARY"))
+                    .fontWeight(.bold) + Text("and try")
+                    .foregroundColor(Color("PRIMARY")) + Text(" WebProtectoror ")
+                    .foregroundColor(Color("PRIMARY")).fontWeight(.bold)
+                Button {
+                  
+                } label: {
+                    NavigationLink(destination: ContentView()) {
+                        ZStack{
+                            Image("bgBtn")
+                            Text("DONE")
+                                .foregroundColor(Color.white)
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                
+            }
+            .frame(maxWidth:.infinity,maxHeight: .infinity,alignment: .top)
+            .padding(.top,100)
+            
+        }
+        .frame(maxWidth:.infinity,maxHeight: .infinity)
+        .background(Color.white).ignoresSafeArea(.all)
+    }
+}
